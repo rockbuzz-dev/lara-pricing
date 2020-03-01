26f410b1fb87052fc44b4d96cc863441ae434914
Tiago Lopes <tiago.desenvolvedorweb@gmail.com>
2020-03-01 15:12:34 -0300

    ajustes

--- src/config/pricing.php
+++ src/config/pricing.php
@@ -1,18 +1,5 @@
 <?php
 
-use Rockbuzz\LaraRbac\Models\{Role, Permission};
-
 return [
-    'models' => [
-        'role' => Role::class,
-        'permission' => Permission::class,
-    ],
-    'tables' => [
-        'prefix' => '',
-        'morph_columns' => [
-            'id' => 'resource_id',
-            'type' => 'resource_type'
-        ]
-    ],
     'positive_values' => ['Y', 'OK', 'TRUE']
 ];
